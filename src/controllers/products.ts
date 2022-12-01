d5aa656da99285a1f0acf59e557b5931075f1d17
graift-off <graf.off.it@gmail.com>
2022-12-01 21:53:08 +0200

    feat: added pagination

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,15 +1,20 @@
 import { Request, Response } from 'express';
 import * as productsServices from '../services/products';
 import { SortType } from '../types/SortType';
+import { PerPageType } from '../types/PerPageType';
 
 export const getAll = async (req: Request, res: Response) => {
   const sort = req.query.sort as SortType || SortType.Newest;
+  const perPage = req.query.perPage as PerPageType || PerPageType.All;
+  const page = Number(req.query.page) || 1;
 
-  const products = await productsServices.getAllByQuery(
+  const productsData = await productsServices.getAllByQuery(
     sort.toLowerCase() as SortType,
+    page,
+    perPage,
   );
 
-  res.send(products);
+  res.send(productsData);
 };
 
 export const getSingle = async (req: Request, res: Response) => {
