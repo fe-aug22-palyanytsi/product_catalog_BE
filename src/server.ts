bdfea9f5d7f706ca7183a7a1a75e7295c4481225
graift-off <graf.off.it@gmail.com>
2022-12-08 16:52:38 +0200

    fix: fixed rotes

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 
 // Deveolp
-// app.use('/products/', productRouter);
+// app.use('/products', productRouter);
 // app.listen(6000, () => {
 //   console.log('Server started at http://localhost:6000');
 // });
